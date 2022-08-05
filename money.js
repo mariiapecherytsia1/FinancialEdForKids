ec7019b8a684ddab4ecf7c12765a50f8fab92b4e
Mariia Pecherytsia <m.pecherytsia@icloud.com>
2022-08-05 00:56:36 -0700

    added new images and pop-ups when shopping

--- money.js
+++ money.js
@@ -1,9 +1,21 @@
-let money = 0;
+let money = 40;
 display();
 function decrementMoney(value)
 {
-    money -= value;
-    console.log("decrementing");
+    if(money - value < 0) {
+        alert("You don't have enough money! Try saving a little more so you can buy more expensive things!");
+    } else if(money - value < 15) {
+        
+        alert("You're running low on money, make sure you earn some more so you don't go broke!");
+        if (confirm("Do you want to save your money? This will make it grow!")) {
+            money -= value;
+            money *= 2;
+        } else {
+            money -= value;
+        }
+    } else {
+        money -= value;
+    }
     display();
 }
 function incrementMoney()
